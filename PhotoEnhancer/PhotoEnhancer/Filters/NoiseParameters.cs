63a76cdfa2910bee58d8337384998d94604a1f57
Kikaboy <56254100+Kikaboy@users.noreply.github.com>
2021-01-12 19:56:31 +0500

    Второе задание
    Выполнено второе задание, 8й вариант.

---
+++ PhotoEnhancer/PhotoEnhancer/Filters/NoiseParameters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class NoiseParameters : IParameters
+    {
+        public double Intensity { get; set; }
+
+        public ParameterInfo[] GetDescription()
+        {
+            return new[]
+            {
+                new ParameterInfo() {
+                    Name = "Интенсивность",
+                    MinValue = 0,
+                    MaxValue = 1,
+                    DefaultValue = 0,
+                    Increment = 0.01
+                    }
+            };
+        }
+
+        public void SetValues(double[] values)
+        {
+            Intensity = values[0];
+        }
+    }
+}
---
+++ PhotoEnhancer/PhotoEnhancer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace PhotoEnhancer
+{
+    static class Program
+    {
+        static Random rnd = new Random();
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            var mainForm = new MainForm();
+
+            mainForm.AddFilter(new PixelFilter<LighteningParameters>(
+                "Осветление/затемнение",
+                (pixel, parameters) => pixel * parameters.Coefficient
+                ));
+
+            mainForm.AddFilter(new PixelFilter<EmptyParameters>(
+                "Оттенки серого",
+                (pixel, parameters) =>
+                {
+                    var chanel = 0.3 * pixel.R +
+                                0.6 * pixel.G +
+                                0.1 * pixel.B;
+
+                    return new Pixel(chanel, chanel, chanel);
+                }
+                ));
+            
+            mainForm.AddFilter(new PixelFilter<NoiseParameters>(
+                "Интенсивность Шума",
+                (originalPixel, parameters) =>
+                {
+                    
+                    var intensity = (parameters as NoiseParameters).Intensity;
+                    return new Pixel(
+                        NoiseGen(originalPixel.R, intensity),
+                        NoiseGen(originalPixel.G, intensity),
+                        NoiseGen(originalPixel.B, intensity));
+
+                    double NoiseGen(double channel, double intensity1)
+                    {
+                        var r = rnd.NextDouble();
+                        return Pixel.Trim((intensity1 * r + (1 - intensity1)) * channel);
+                    }
+
+                    
+                }
+                ));
+
+            mainForm.AddFilter(new TransformFilter(
+                "Отражение по вертикали",
+                size => size,
+                (point, size) => new Point(point.X - size.Width + 420, size.Height - 1 - point.Y)));
+
+            mainForm.AddFilter(new TransformFilter<HorizontalTiltParameters>(
+            "Скос по горизонтали", new HorizontalTiltTransformer()));
+
+            Application.Run(mainForm);
+        }
+    }
+}
