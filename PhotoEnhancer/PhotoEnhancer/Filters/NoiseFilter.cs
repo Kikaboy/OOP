9b4ff688c1c4071a98fb24b95a727ff9f8c56e5e
Kikaboy <56254100+Kikaboy@users.noreply.github.com>
2021-01-12 17:11:50 +0500

    Fixed
    Исправление по предложенным изменениям.

--- PhotoEnhancer/PhotoEnhancer/Filters/NoiseFilter.cs
+++ PhotoEnhancer/PhotoEnhancer/Filters/NoiseFilter.cs
@@ -2,11 +2,13 @@
 using System.Drawing;
 using System.Windows.Forms;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace PhotoEnhancer
 {
     public class NoiseFilter : PixelFilter<NoiseParameters>
     {
+        static Random rnd = new Random();
         public override string ToString()
         {
             return "Интенсивность шума";
@@ -15,19 +17,15 @@ namespace PhotoEnhancer
         public override Pixel ProcessPixel(Pixel originalPixel,
            NoiseParameters parameters)
         {
-            var red = originalPixel.R;
-            var green = originalPixel.G;
-            var blue = originalPixel.B;
-            var rnd = new Random();
-            for (var i = 0; i < 340; i++)
-            {
+            double red, green, blue;
+            
                 var r = (rnd.NextDouble());
                 red = (parameters.Intensity * r + (1 - parameters.Intensity) * originalPixel.R);
                 var r2 = (rnd.NextDouble());
                 green = (parameters.Intensity * r2 + (1 - parameters.Intensity) * originalPixel.G);
                 var r3 = (rnd.NextDouble());
                 blue = (parameters.Intensity * r3 + (1 - parameters.Intensity) * originalPixel.B);
-            }
+
             return new Pixel(red, green, blue);
 
         }
